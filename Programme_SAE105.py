d692fec854cce1e8db2b77111c07f03542fdfe4b
ne725072 <nabil.el-khallouqi@iut-dijon.u-bourgogne.fr>
2026-01-17 13:18:51 +0100

    Add files via upload

--- Programme_SAE105.py
+++ Programme_SAE105.py
@@ -29,10 +29,10 @@ del conso_corrigee[0]
 i_supr = []
 
 for i in range(len(conso_corrigee)):
-    if conso_corrigee[i] == 'NA' :
+    if 'NA' in conso_corrigee[i]:
        i_supr.append(i)
      
-for i in range(len(i_supr)-1,1,-1) :
+for i in range(len(i_supr)-1,-1,-1) :
     
     del conso_corrigee[i_supr[i]]
     del maille[i_supr[i]]
@@ -40,12 +40,11 @@ for i in range(len(i_supr)-1,1,-1) :
     del semaines[i_supr[i]]
     del annees[i_supr[i]]
     del donnees[i_supr[i]]
-               
-               
+            
 for i in range(len(conso_corrigee)):
         conso_corrigee[i] = conso_corrigee[i].replace(",",".")
         conso_corrigee[i] = float(conso_corrigee[i])
-        print(conso_corrigee[i])
+
 
 indice_quotidien = []
 indice_hebdo = []
@@ -66,27 +65,46 @@ conso_corrigee_hebdo = []
 
 for i in indice_hebdo :
     conso_corrigee_hebdo.append(conso_corrigee[i])
-    
+            
+indice_2025 = []
+for i in range(len(dates)):
+    if dates[i][0] == '2025':
+        indice_2025.append(i)
+
 for i in range(len(dates)):
     dates[i] = dates[i].split('-')
-        
-dates_2024 = []
-dates_2025 = []     
+
+dates_2025_hebdo = []
+conso_2025_hebdo = []
 
 for i in range(len(dates)):
-    for j in dates[i]:
-        if dates[i][0] == '2024':
-            dates_2024.append(dates[i])
-        if dates[i][0] == '2025':
-            dates_2025.append(dates[i])
+    if i in indice_hebdo and dates[i][0] == '2025':
+        dates_2025_hebdo.append(dates[i])
+
+for i in range(len(conso_corrigee_hebdo)):
+    if dates[i][0] == '2025':
+        conso_2025_hebdo.append(conso_corrigee_hebdo[i])
+
+liste = []
+
+for i in range(len(dates_2025_hebdo)):
+    temp = int(dates_2025_hebdo[i][0] + dates_2025_hebdo[i][1] + dates_2025_hebdo[i][2])
+    liste.append([temp, dates_2025_hebdo[i], conso_2025_hebdo[i]])
+
+liste.sort()
+
+dates_2025_hebdo_triee = []
+conso_2025_hebdo_triee = []
+
+for i in range(len(liste)):
+    dates_2025_hebdo_triee.append(liste[i][1])
+    conso_2025_hebdo_triee.append(liste[i][2])
+    
+for i in range(len(dates_2025_hebdo_triee)):
+    dates_2025_hebdo_triee[i] = dates_2025_hebdo_triee[i][0] + '-' + dates_2025_hebdo_triee[i][1] + '-' + dates_2025_hebdo_triee[i][2] 
 
 
-
-
-'''
 pylab.figure()
-pylab.plot()
-pylab.plot()
+pylab.plot(dates_2025_hebdo_triee, conso_2025_hebdo_triee, label = 'Consommation hebdomadaire 2025')
 pylab.legend()
 pylab.show()
-'''
